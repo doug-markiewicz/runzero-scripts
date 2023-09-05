6849ede1b9b850924ecc38c9272c02c739633cf7
Doug Markiewicz <doug.markiewicz@gmail.com>
2023-09-05 09:38:55 -0400

    Update tanium scripts and add config file

--- sdk/tanium.py
+++ sdk/tanium.py
@@ -2,29 +2,29 @@ import requests
 import os
 import uuid
 import json
-import datetime
+from datetime import datetime
 from flatten_json import flatten
 from ipaddress import ip_address
 from typing import Any, Dict, List
-
 import runzero
 from runzero.client import AuthError
 from runzero.api import CustomAssets, Sites
 from runzero.types import (CustomAttribute,ImportAsset,IPv4Address,IPv6Address,NetworkInterface,ImportTask)
+import tanium_config
 
 # Configure runZero variables
-RUNZERO_BASE_URL = 'https://console.runZero.com/api/v1.0'
-RUNZERO_ORG_ID = ''
-RUNZERO_SITE_NAME = ''
-RUNZERO_SITE_ID = ''
-RUNZERO_CUSTOM_SOURCE_ID = '' 
-RUNZERO_IMPORT_TASK_NAME = 'Tanium Sync'
-RUNZERO_CLIENT_ID = ''
-RUNZERO_CLIENT_SECRET = ''
+RUNZERO_BASE_URL = tanium_config.RUNZERO_BASE_URL
+RUNZERO_ORG_ID = tanium_config.RUNZERO_ORG_ID
+RUNZERO_SITE_NAME = tanium_config.RUNZERO_SITE_NAME
+RUNZERO_SITE_ID = tanium_config.RUNZERO_SITE_ID
+RUNZERO_CUSTOM_SOURCE_ID = tanium_config.RUNZERO_CUSTOM_SOURCE_ID
+RUNZERO_IMPORT_TASK_NAME = tanium_config.RUNZERO_IMPORT_TASK_NAME
+RUNZERO_CLIENT_ID = tanium_config.RUNZERO_CLIENT_ID
+RUNZERO_CLIENT_SECRET = tanium_config.RUNZERO_CLIENT_SECRET
 
 # Configure Tanium variables
-TANIUM_API_GATEWAY = 'https://your-account-api.cloud.tanium.com/plugin/products/gateway/graphql'
-TANIUM_API_TOKEN = 'token-1234567890abcdefghijklmnopqrstuvwxyz'
+TANIUM_API_GATEWAY = tanium_config.TANIUM_API_GATEWAY
+TANIUM_API_TOKEN = tanium_config.TANIUM_API_TOKEN
 
 def build_assets_from_json(json_input: List[Dict[str, Any]]) -> List[ImportAsset]:
     '''
@@ -32,46 +32,60 @@ def build_assets_from_json(json_input: List[Dict[str, Any]]) -> List[ImportAsset
     is required for uploading to the runZero platform. This function assumes that the json has been converted into a list 
     of dictionaries using `json.loads()` (or any similar functions).
     '''
-
     assets: List[ImportAsset] = []
     for i in json_input:
         item = i["node"]
         # grab known API attributes from the json dict that are always present
         tanium_oid = item.get('id', uuid.uuid4)
         tanium_hostname = item.get('name', '')
-        tanium_mac = item.get('macAddresses_0', '')
+        tanium_macs = item.get('macAddresses', [])
         tanium_address = item.get('ipAddress', '')
         tanium_domain = item.get('domainName', '')
         tanium_first_seen = item.get('eidFirstSeen', '')
-        tanium_last_seen = item.get('eidLastSeen', '')
-        tanium_os_name = item.get('os_name', '')
+        tanium_os_name = item.get('os', {}).get('name', '')
         tanium_manufacturer = item.get('manufacturer', '')
         tanium_model = item.get('model', '')
 
-        # create the network interface
-        network = build_network_interface(ips=[tanium_address], mac=tanium_mac)
-        print(network)
-
-        # *** DO NOT TOUCH ***
+        # create network interfaces
+        networks = []
+        if len(tanium_macs) > 0 and isinstance(tanium_macs, list):
+            for mac in tanium_macs:
+                if len(mac) <= 23:
+                    network = build_network_interface(ips=[tanium_address], mac=mac)
+                    networks.append(network)
+                else:
+                    network = build_network_interface(ips=[tanium_address], mac=None)
+                    networks.append(network)
+        else:
+            network = build_network_interface(ips=[tanium_address], mac=None)
+            networks.append(network)
+        
         # handle any additional values and insert into custom_attrs
         custom_attrs: Dict[str, CustomAttribute] = {}
-        for key, value in item.items():
-            if isinstance(value, dict):
-                for k, v in value.items():
-                    custom_attrs[k] = CustomAttribute(str(v)[:1023])
-            else:
-               custom_attrs[key] = CustomAttribute(str(value))
 
-        # Build assets for import
+        root_keys_to_ignore = []
+        for key, value in item.items():
+            if not isinstance(value, dict):
+                root_keys_to_ignore.append(key)
+
+        flattened_items = flatten(nested_dict=item,
+                                  root_keys_to_ignore=root_keys_to_ignore)
+
+        item = flattened_items | item
+
+        for key, value in item.items():
+            if not isinstance(value, dict):
+                custom_attrs[key] = CustomAttribute(str(value)[:1023])
+
+        # Build assets for import 
         assets.append(
             ImportAsset(
                 id=tanium_oid,
-                networkInterfaces=[network],
+                networkInterfaces=networks,
                 hostnames=[tanium_hostname],
                 domain=tanium_domain,
                 os=tanium_os_name,
-                first_seen_ts=tanium_first_seen,
-                last_seen_ts=tanium_last_seen,
+                firstSeenTS=tanium_first_seen,
                 manufacturer=tanium_manufacturer,
                 model=tanium_model,
                 customAttributes=custom_attrs
@@ -79,7 +93,6 @@ def build_assets_from_json(json_input: List[Dict[str, Any]]) -> List[ImportAsset
         )
     return assets
 
-# *** DO NOT TOUCH ***
 def build_network_interface(ips: List[str], mac: str = None) -> NetworkInterface:
     ''' 
     This function converts a mac and a list of strings in either ipv4 or ipv6 format and creates a NetworkInterface that
@@ -88,16 +101,19 @@ def build_network_interface(ips: List[str], mac: str = None) -> NetworkInterface
     ip4s: List[IPv4Address] = []
     ip6s: List[IPv6Address] = []
     for ip in ips[:99]:
-        ip_addr = ip_address(ip)
-        if ip_addr.version == 4:
-            ip4s.append(ip_addr)
-        elif ip_addr.version == 6:
-            ip6s.append(ip_addr)
-        else:
+        try:
+            ip_addr = ip_address(ip)
+            if ip_addr.version == 4:
+                ip4s.append(ip_addr)
+            elif ip_addr.version == 6:
+                ip6s.append(ip_addr)
+            else:
+                continue
+        except:
             continue
-    if mac is None:
+    if mac is None or not isinstance(mac, str) or mac == "[no results]":
         return NetworkInterface(ipv4Addresses=ip4s, ipv6Addresses=ip6s)
-    else:
+    else:     
         return NetworkInterface(macAddress=mac, ipv4Addresses=ip4s, ipv6Addresses=ip6s)
 
 
@@ -123,18 +139,6 @@ def import_data_to_runzero(assets: List[ImportAsset]):
         print(f'unable to find requested site')
         return
 
-    # (Optional)
-    # Check for custom source and create new one if it doesn't exist
-    '''
-    custom_source_mgr = CustomSourcesAdmin(c)
-    my_asset_source = custom_source_mgr.get(name='fortiedr')
-    if my_asset_source:
-        source_id = my_asset_source.id
-    else:
-        my_asset_source = custom_source_mgr.create(name='fortiedr')
-        source_id = my_asset_source.id
-    '''
-
     # create the import manager to upload custom assets
     import_mgr = CustomAssets(c)
     import_task = import_mgr.upload_assets(org_id=RUNZERO_ORG_ID, site_id=RUNZERO_SITE_ID, custom_integration_id=RUNZERO_CUSTOM_SOURCE_ID, assets=assets, task_info=ImportTask(name=RUNZERO_IMPORT_TASK_NAME))
@@ -158,6 +162,7 @@ def main():
                     macAddresses
                     os {
                         name
+                        generation
                     }
                     model
                     manufacturer
@@ -188,34 +193,26 @@ def main():
 
         # Set query variables
         if cursor:
-            variables = {"first": 100, "after": cursor}
+            variables = {"first": 1000, "after": cursor}
         else:
-            variables = {"first": 100}
+            variables = {"first": 1000}
 
         # Request batch of endpoints
         endpoints = requests.post(url=TANIUM_API_GATEWAY, headers=query_headers, json={'query': query, 'variables': variables})
 
         # Check for 200 OK response code and exit if error
         if endpoints.status_code != 200:
-            print("Response: " + str(endpoints.status_code) + " " + endpoints.text)
+            print("Tanium Response: " + str(endpoints.status_code) + " " + endpoints.text)
             exit(endpoints.status_code)
         
         # Parse JSON object to native python object
         endpoints_json = endpoints.json()["data"]["endpoints"]["edges"]
-        
-        # Print endpoints for troubleshooting
-        #print(json.dumps(endpoints.json(),indent=2))
-
-        for a in endpoints_json:
-            for k in a.keys():
-                if isinstance(a[k], dict):
-                    a[k] = flatten(a[k])
-
+                    
         # Extract page info from 
         hasNextPage = endpoints.json()["data"]["endpoints"]["pageInfo"]["hasNextPage"]
         startCursor = endpoints.json()["data"]["endpoints"]["pageInfo"]["startCursor"]
         endCursor = endpoints.json()["data"]["endpoints"]["pageInfo"]["endCursor"]
-        print(str(datetime.datetime.now()) + f" Received data for endpoints between {startCursor} and {endCursor}")
+        print(str(datetime.now()) + f" Received data for endpoints between {startCursor} and {endCursor}")
         
         endpoint_array.extend(endpoints_json)
 
@@ -223,13 +220,12 @@ def main():
         cursor = endCursor
 
     # Format asset list for import into runZero
-    print(str(datetime.datetime.now()) + f" Building assets for import into runZero.")
+    print(str(datetime.now()) + f" Building assets for import into runZero.")
     import_assets = build_assets_from_json(endpoint_array)
 
     # Import assets into runZero
-    print(str(datetime.datetime.now()) + f" Importing assets into runZero.")
+    print(str(datetime.now()) + f" Importing assets into runZero.")
     import_data_to_runzero(assets=import_assets)
 
-# *** DO NOT TOUCH ***
 if __name__ == '__main__':
     main()
